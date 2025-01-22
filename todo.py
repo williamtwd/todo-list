1afa121b7769018f2448fbbbb9a0a0753a7c93d2
Collabun <adresse@email.com>
2025-01-22 11:16:49 +0100

    Développement de la fonctionnalité supprimer une tâche

--- todo.py
+++ todo.py
@@ -9,16 +9,23 @@ def lister_taches():
     for i, tache in enumerate(taches, 1):
         print(f"{i}. {tache}")
 
+def suptache():
+	iddel = input("Choisissez la tâche à supprimer")
+	del taches[iddel]
+	print("La tâche n°" + iddel + " a été supprimée")
+
 def main():
     while True:
-        print("\nOptions : [1] Ajouter une tâche [2] Lister les tâches [3] Quitter")
+        print("\nOptions : [1] Ajouter une tâche [2] Lister les tâches [3] Supprimer une tâche [4] Quitter")
         choix = input("Choisissez une option : ")
         if choix == "1":
             tache = input("Entrez une tâche : ")
             ajouter_tache(tache)
         elif choix == "2":
             lister_taches()
-        elif choix == "3":
+	elif choix == "3":
+		suptache()
+        elif choix == "4":
             print("Au revoir !")
             break
         else:
